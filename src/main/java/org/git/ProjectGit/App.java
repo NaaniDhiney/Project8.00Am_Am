dec408a66fc4bd0d38569d937f5cd95a465bbf35
NaaniDhiney <Dineshabdulraj@gmail.com>
2022-12-21 20:45:59 +0530

    Abdul Codes

--- src/main/java/org/git/ProjectGit/App.java
+++ src/main/java/org/git/ProjectGit/App.java
@@ -3,12 +3,20 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Hello World!" );
-    	System.out.println( "Hello World!" );
+    	System.out.println( "Hello !" );
+    	System.out.println( "Hello Wor!" );
+        System.out.println( "Hello W!" );
+        System.out.println( "Hello Worl!" );
+        System.out.println( "Hello Wo!" );
+        System.out.println( "Hello Wrld!" );
+        System.out.println( "Hello orld!" );
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Wld!" );
+        System.out.println( "Hello Wrld!" );
+        System.out.println( "Hello rld!" );
+        System.out.println( "Hello Wold!" );
+        System.out.println( "Hello Wo!" );
+        System.out.println( "Hello Wol!" );
+        
     }
 }
