8ab0aa4961cf8a513f1188bd8d329676a91097e8
NaaniDhiney <Dineshabdulraj@gmail.com>
2022-12-26 15:43:39 +0530

    new dinesh

--- src/main/java/org/git/ProjectGit/App.java
+++ src/main/java/org/git/ProjectGit/App.java
@@ -8,5 +8,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        
+        System.out.println("hello");
     }
 }
